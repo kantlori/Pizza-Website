f2f2f75aa41d744d286995d6cf0211664702a906
kantlori <atabeywade@gmail.com>
2024-12-30 22:31:04 +0300

    header css upgrade

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { debounce } from 'lodash';
 
 function Header({ onBack }) {
+
+    const [isShrink, setIsShrink] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = debounce(() => {
+            if (window.scrollY > 1) {
+                setIsShrink(true);
+            } else {
+                setIsShrink(false);
+            }
+        });
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <header>
+        <header className={`${isShrink ? 'shrink' : ''}`}>
             <img src="../../images/iteration-1-images/logo.svg" alt="" data-cy="header-logo" />
             <div className="order-header-buttons">
                 <button onClick={onBack} data-cy="returnhome-button">Anasayfa</button>
